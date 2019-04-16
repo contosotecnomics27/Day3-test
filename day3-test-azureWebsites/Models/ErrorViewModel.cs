3186e113e03e72cdcc8d38987a1f25d708d9bf0e
contosoTecnomics@hotmail.com <contosoTecnomics@hotmail.com>
2019-04-17 01:04:53 +0500

    Add project files.

---
+++ day3-test-azureWebsites/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace day3_test_azureWebsites.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
